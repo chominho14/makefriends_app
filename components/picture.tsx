56a5fc73e67152a457223e3b5897cc503820af76
chominho14 <chominho14@naver.com>
2022-05-02 13:00:15 +0900

    v1.1.7(Components - picture heart - div tag)

--- components/picture.tsx
+++ components/picture.tsx
@@ -26,24 +26,24 @@ export default function Picture({
               {age}세, {mbti}
             </span>
           </div>
-          <div className="flex items-end justify-end space-x-2">
-            <div className="flex items-center space-x-0.5 text-sm  text-gray-600">
-              <svg
-                className="h-4 w-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-              <span>{hearts}</span>
-            </div>
+        </div>
+        <div className="flex items-end justify-end space-x-2">
+          <div className="flex items-center space-x-0.5 text-sm  text-gray-600">
+            <svg
+              className="h-4 w-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+            <span>{hearts}</span>
           </div>
         </div>
       </a>
