f215c06f13c649389bc7e76c0dd26a683a11cc46
chominho14 <chominho14@naver.com>
2022-05-20 15:00:25 +0900

    v1.2.9(Profile - Loved Page - Components and Front pages)

---
+++ components/picture-list.tsx
@@ -0,0 +1,34 @@
+import { ProductWithCount } from "pages";
+import useSWR from "swr";
+import Picture from "./picture";
+
+interface PictureListProps {
+  kind: "favs";
+}
+
+interface Record {
+  id: number;
+  product: ProductWithCount;
+}
+
+interface ProductListResponse {
+  [key: string]: Record[];
+}
+
+export default function PictureList({ kind }: PictureListProps) {
+  const { data } = useSWR<ProductListResponse>(`/api/users/me/${kind}`);
+  return data ? (
+    <>
+      {data[kind]?.map((record) => (
+        <Picture
+          id={record.product.id}
+          key={record.id}
+          title={record.product.name}
+          age={record.product.age}
+          mbti={record.product.mbti}
+          hearts={record.product._count.favs}
+        />
+      ))}
+    </>
+  ) : null;
+}
--- pages/profile/loved.tsx
+++ pages/profile/loved.tsx
@@ -1,21 +1,14 @@
 import type { NextPage } from "next";
 import Item from "@components/picture";
 import Layout from "@components/layout";
+import Picture from "@components/picture";
+import PictureList from "@components/picture-list";
 
 const Loved: NextPage = () => {
   return (
     <Layout title="관심목록" canGoBack>
-      <div className="flex flex-col space-y-5 pb-10  divide-y">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            key={i}
-            id={i}
-            title="iPhone 14"
-            age={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
+      <div className="flex flex-col space-y-5 divide-y  pb-10">
+        <PictureList kind="favs" />
       </div>
     </Layout>
   );
