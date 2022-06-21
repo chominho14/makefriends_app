05bde241ac4c659be0e0fd33a18fd2e8911ac929
chominho14 <chominho14@naver.com>
2022-06-21 14:36:23 +0900

    v1.4.1(NextJS - Middleware - NextReqeust and NextResponse)

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,12 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua);
-  console.log("global middleware");
+  if (req.ua?.isBot) {
+    return new Response("Plz don`t be a bot. Be human.", { status: 403 });
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.mynextapp) {
+      return NextResponse.redirect("/enter");
+    }
+  }
 }
